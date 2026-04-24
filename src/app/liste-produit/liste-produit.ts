d7accd93988862bdf4bb8c1427469447cf85eaf3
abdelhak-kadir <abdelhakaitkadir03@gmail.com>
2026-04-24 13:12:54 +0100

    feat: enhance product listing and detail views with routing and improved layout

--- src/app/liste-produit/liste-produit.ts
+++ src/app/liste-produit/liste-produit.ts
@@ -2,9 +2,11 @@ import { Component ,Input ,inject, OnInit} from '@angular/core';
 import { Produit } from '../interface_produits';
 import { CommonModule } from '@angular/common';
 import { ServiceProduit } from '../service-produit';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-liste-produit',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './liste-produit.html',
   styleUrl: './liste-produit.css',
 })
--- src/app/produit/detail-produit/detail-produit.ts
+++ src/app/produit/detail-produit/detail-produit.ts
@@ -1,8 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ServiceProduitService } from '../service-produit.service';
-import { Produit } from '../produit';
+import { ServiceProduit } from '../../service-produit';
+import { Produit } from '../../interface_produits';
 @Component({
   selector: 'app-detail-produit',
   imports: [CommonModule],
@@ -11,9 +11,9 @@ import { Produit } from '../produit';
 })
 export class DetailProduit implements OnInit {produit: Produit | undefined;
   private route = inject(ActivatedRoute);
-  private produitService = inject(ServiceProduitService);
+  private produitService = inject(ServiceProduit);
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.produit = this.produitService.getProduits().find(p => p.id === id);
+    this.produit = this.produitService.getProducts().find(p => p.id === id);
   }}
