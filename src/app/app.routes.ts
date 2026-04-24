956c9e9e8ea4650bd04ed6487b76dfec213f7289
abdelhak-kadir <abdelhakaitkadir03@gmail.com>
2026-04-24 12:47:42 +0100

    feat: implement login functionality and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { ListeProduit } from './liste-produit/liste-produit';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'produits', component: ListeProduit },
+  { path: '**', component: NotFound }
+];
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email: string = '';
+  password: string = '';
+  error: string = '';
+
+  private router = inject(Router);
+
+  login(): void {
+    if (this.email === 'admin@shop.com' && this.password === '1234') {
+      this.router.navigate(['/produits']);
+    } else {
+      this.error = 'Email ou mot de passe incorrect !';
+    }
+  }
+}
